8fa8003378f8940fffde8b21b32792b853a91ce9
hyojelee <hyojelee1004ss@gmail.com>
2020-08-09 02:56:25 -0400

    login added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 const config = require("./config/key");
 
@@ -10,6 +11,7 @@ const config = require("./config/key");
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // useNewUrlParser: true, to remove deprecate warning
 //   useUnifiedTopology: true,
@@ -36,6 +38,36 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+app.post("/login", (req, res) => {
+  // find a requested email from database
+
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "your email is invaild",
+      });
+    }
+    // if there is email from database check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "your password is invalid",
+        });
+    });
+    // if password and email are correct, make token
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+
+      //save token into cookie, but i can save other place too, like local or session storage
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -22,6 +25,45 @@ const userSchema = mongoose.Schema({
   token: { type: String },
   tokenExp: { type: Number },
 });
+// before saving data to DB, do password encrypt
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plain password==1234, database password=ASD!@#!!S
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    // else
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  // make token
+  var token = jwt.sign(user._id.toHexString(), "userToken");
+  usertoken = token;
+
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
